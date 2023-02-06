984fbaa62c96b4415a45f641df5a10f9fd3841fe
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-02-06 19:36:23 +0100

    Inicio del proyecto

---
+++ src/database/database.conexion.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+
+const db = `${process.env.DATABASE}`;
+const user = `${process.env.USERDB}`;
+const pass = `${process.env.PASSWORDDB}`;
+
+export const sequelize = new Sequelize(db, user, pass, {
+    host: 'localhost',
+    dialect: 'postgres'
+});
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import app from './app';
+import { sequelize } from './database/database.conexion';
+
+const PORT = process.env.PORT || 3002;
+
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+    } catch (error) {
+        console.error('Error en la conexión a la BBDD:', error);
+    }
+}
+
+main();
+
