94ad5f11444d4dc8ec321f256719621a58bba918
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-02-07 11:05:02 +0100

    Modelos

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import 'dotenv/config';
 import app from './app';
 import { sequelize } from './database/database.conexion';
 
+// TODO: delete model's imports.
+import './models/category.model';
+import './models/password.model';
+import './models/user.model';
+
 const PORT = process.env.PORT || 3002;
 
 const main = async () => {
     try {
-        await sequelize.authenticate();
+        await sequelize.sync({ force: true }); // TODO: force false.
         app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
     } catch (error) {
         console.error('Error en la conexión a la BBDD:', error);
@@ -14,4 +19,3 @@ const main = async () => {
 }
 
 main();
-
---
+++ src/models/category.model.ts
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.conexion' // Conexion to DDBB.
+import { passwordModel } from './password.model';
+
+export const categoryModel = sequelize.define('categories', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+//*Relationships
+
+categoryModel.hasMany(passwordModel, {
+    foreignKey: 'categoryId',
+    sourceKey: 'id'
+});
+
+passwordModel.belongsTo(categoryModel, {
+    foreignKey: 'categoryId',
+    targetKey: 'id'
+});
