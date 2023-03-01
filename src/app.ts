9c8d526835de2a9c00510e3c7f8aff4410021bdf
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-03-01 19:39:09 +0100

    Primera prueba de cors

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use((req, res, next) => {
+    console.log('CORS middleware loaded successfully');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
