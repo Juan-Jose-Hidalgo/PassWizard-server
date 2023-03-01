3577585ed0604cbc0b7d81cc758c834ba7df0fd0
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-03-01 19:29:52 +0100

    Configuración de cors provisional para entorno de pruebas local

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(cors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -20,7 +26,6 @@ app.use(cors({
 }));
 app.use(morgan('dev'));
 app.use('/uploads', express.static(path.resolve('uploads')));
-app.use(router);
-app.use('/ping', (req, res) => res.send('pong'));
+app.use(router); //Aquí cargo las rutas de manera dinámica.
 
 export default app;
