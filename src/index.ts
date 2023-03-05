390883e0da553bd9b316f7c7075d3ed2d7e75500
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-03-05 14:42:21 +0100

    Corregido un error al eliminar la imagen de perfil

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { sequelize } from './database/database.conexion';
 const PORT = process.env.PORT || 3002;
 const main = async () => {
     try {
-        await sequelize.sync({ force: true });
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
     } catch (error) {
         console.error('Error en la conexión a la BBDD:', error);
